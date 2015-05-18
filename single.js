dc7676f214f94901d1b4c6e9587554ccdb4642ed
whoopeedeedoo <whoopeedeedoo@gmail.com>
2015-05-18 21:37:48 +0800

    add single ver.

--- single.js
+++ single.js
@@ -41,16 +41,8 @@ async.parallel([
 			'Cookie': '_gat=1; _wootalk_session=' + results[0] + '; _ga=GA1.2.1804571589.1429605824; __asc=6c4424fc14ce5fe7639ea11437a; __auc=c71404c914cdb259f913b23fc5b'
 		})
 	});
-	wsB = new WebSocket('wss://wootalk.today/websocket', [], {
-		headers: _.extend(wootalk_header, {
-			'Cookie': '_gat=1; _wootalk_session=' + results[1] + '; _ga=GA1.2.1804571589.1429605824; __asc=6c4424fc14ce5fe7639ea11437a; __auc=c71404c914cdb259f913b23fc5b'
-		})
-
-	});
-
 
 	var userId_A = null;
-	var userId_B = null;
 	process.stdin.setEncoding('utf8');
 	process.stdin.on('readable', function(){
 		var input = process.stdin.read();
@@ -59,13 +51,15 @@ async.parallel([
 		//var sample = ["new_message",{"id":null,"channel":null,"user_id":845609,"data":{"sender":1,"message":"å®å®","time":1431750704164,"msg_id":1},"success":null,"result":null,"token":null,"server_token":null}];
 		var sample = ["new_message",{"id":null,"channel":null,"user_id":876225,"data":{"sender":1,"message":"對","time":1431952068796,"mobile":null},"success":null,"result":null,"token":null,"server_token":null}];
 		if(input){
-			var sendToWho = input.substring(0, 3);
-			var content =  input.substring(4, input.length-1);
+			//var sendToWho = input.substring(0, 3);
+			//var content =  input.substring(4, input.length-1);
+			var content = input.substring(0, input.length-1);
 		}
-		if(sendToWho == 'end'){
+		if(content == 'end'){//sendToWho
 			console.log('process.exit()');
+			wsA.send(leavecmd);
 			process.exit();
-		}else if(sendToWho == 'toa' && userId_A ){
+		}else if( userId_A ){ //sendToWho == 'toa' &&
 			console.log('發話給A用的user_id: '+userId_A);
 			console.log('代替輸入send to A: '+content);
 			//temp = fakePaToA;
@@ -75,16 +69,6 @@ async.parallel([
 			fakeMessage = JSON.stringify(sample);
 			console.log(fakeMessage);
 			wsA.send(fakeMessage);
-		}else if(sendToWho == 'tob' && userId_B ){
-			console.log('發話給B用的user_id: '+userId_B);
-			console.log('代替輸入send to B: '+content);
-			//temp = fakePaToB;
-			sample[1]['user_id'] = userId_B;
-			sample[1]['data']['message'] = content;
-			//temp[1]['data']['message'] = content;
-			fakeMessage = JSON.stringify(sample);
-			console.log(fakeMessage);
-			wsB.send(fakeMessage);
 		}else{
 			console.log('尚未取得足夠對話參數')
 		}
@@ -119,7 +103,7 @@ async.parallel([
 				//fakePaToB = pa;//取得傳假話參數");
 				console.log("A：「 " + msg + " 」");
 				console.log(message);
-				wsB.send(message);
+				//wsB.send(message);
 			} else if (!sender && leave) {
 				//leave == false 是初始系統提示訊息的時候, 其餘時候都是undefined
 				//change person 或 disconnected
@@ -127,7 +111,7 @@ async.parallel([
 				console.log('A 已經離開房間');
 
 				wsA.send(leavecmd);
-				wsB.send(leavecmd);
+				//wsB.send(leavecmd);
 			}
 		} else if (ev == 'update_state') {
 
@@ -137,7 +121,7 @@ async.parallel([
 			if (pa[1]['data']['last_read']) {
 				//console.log('A 已讀');
 			}
-			wsB.send(message);
+			//wsB.send(message);
 
 
 		} else if (ev == 'websocket_rails.ping') {
@@ -155,60 +139,5 @@ async.parallel([
 
 
 
-	wsB.on('open', function() {
-		// console.log('B connected!');
-	});
-	wsB.on('message', function(message) {
-		var pa = JSON.parse(message)[0]; //parse
-		var ev = pa[0]; //event名字
-		/*
-			client_connected, new_message, websocket_rails.ping, update_state
-		*/
-		var msg = pa[1]['data']['message'];
-		var sender = pa[1]['data']['sender']; //0 是系統, 1是自己, 2是對方
-		var leave = pa[1]['data']['leave']; //若對方leave, 要寄給系統["change_person",{}]
-		if (ev == 'new_message') {
-			//console.log(message)
-			if( pa[1]['user_id'] ){
-				userId_B = pa[1]['user_id'];
-			}
-			pa[1]['data']['sender'] = 1; //使系統知道是我要傳給對方
-			message = JSON.stringify(pa);
-			if (sender == 2) {
-				//fakePaToA = pa;//取得傳假話參數
-				console.log("B：「 " + msg + " 」\n");
-				console.log(message);
-				wsA.send(message);
-			} else if (!sender && leave) {
-				//leave == false 是初始系統提示訊息的時候, 其餘時候都是undefined
-				//change person 或 disconnected
-				console.log('B 已經離開房間');
-
-				wsA.send(leavecmd);
-				wsB.send(leavecmd);
-			}
-		} else if (ev == 'update_state') {
-			if (pa[1]['data']['typing']) {
-				//console.log('B typing...');
-			}
-			if (pa[1]['data']['last_read']) {
-				//console.log('B 已讀');
-			}
-			wsA.send(message);
-
-
-		} else if (ev == 'websocket_rails.ping') {
-			a = Randomize();
-			wsB.send('["websocket_rails.pong",{"id":' + Randomize() + ',"data":{}}]')
-
-		} else if (ev == 'client_connected') {
-			console.log('B 已進入房間');
-			// wsB.send('["new_message",{"id":70527,"data":{"message":"嗨","msg_id":1}}]')
-		}
-	});
-
-	wsB.on('close', function() {
-		// console.log('B disconnected');
-	});
 
 });
